a5ed51455864fc8e8614979a7e05bb03306a3393
Den1xxx <from.gomel@gmail.com>
2014-03-15 09:59:19 +0300

    Change "Menus management"
    В управление «Модулями меню» изменена концепция редактирования.
    Теперь не придётся кликать мышкой, чтобы переместить модуль вниз или
    вверх.
    Теперь нужно просто перетащить его — куда требуется.
    И не забудьте сохраниться.

--- admin/modules/modules/menus.php
+++ admin/modules/modules/menus.php
@@ -1,8 +1,8 @@
 <?php
 ////////////////////////////////////////////////////////////////////////////////
-//   Copyright (C) ReloadCMS Development Team                                 //
-//   http://reloadcms.com                                                     //
-//   This product released under GNU General Public License v2                //
+//   Copyright (C) ReloadCMS Development Team                  //
+//   http://reloadcms.com                            //
+//   This product released under GNU General Public License v2           //
 ////////////////////////////////////////////////////////////////////////////////
 rcms_loadAdminLib('ucm');
 
@@ -12,11 +12,11 @@ if(!empty($_POST['save'])){
     if(!empty($_POST['menus'])){
     	foreach ($_POST['menus'] as $element){
         	if(substr($element, 0, 1) == '/') {
-	            $content .= '[' . substr($element, 1) . "]\n";
+	       $content .= '[' . substr($element, 1) . "]\n";
     	        $i = 0;
 	        } elseif($i !== -1) {
-            	$content .= $i . '=' . $element . "\n";
-            	$i++;
+       	$content .= $i . '=' . $element . "\n";
+       	$i++;
         	}
     	}
     }
@@ -24,74 +24,95 @@ if(!empty($_POST['save'])){
 }
 
 /******************************************************************************
-* Interface                                                                   *
+* Interface                                     *
 ******************************************************************************/
 $menus = parse_ini_file(CONFIG_PATH . 'menus.ini', true);
+$subm = '&nbsp;&nbsp;&nbsp;> ';
 include(SKIN_PATH . $system->skin . '/skin.php');
 $current = array();
-$usused = array();
 foreach ($menus as $column => $coldata){
     if(!empty($skin['menu_point'][$column])){
-        $current['/' . $column] = __('Column') . ': ' . $skin['menu_point'][$column];
+        $current['/' . $column] = '<b>'.__('Column') . ': ' . $skin['menu_point'][$column].'</b>';
         foreach ($coldata as $menu){
-            if(substr($menu, 0, 4) == 'ucm:' && is_readable(DF_PATH . substr($menu, 4) . '.ucm')) {
-                $current[$menu] = ' > ' . $menu;
-            } elseif (!empty($system->modules['menu'][$menu])) {
-                $current[$menu] = ' > ' . $system->modules['menu'][$menu]['title'];
-            }
+       if(substr($menu, 0, 4) == 'ucm:' && is_readable(DF_PATH . substr($menu, 4) . '.ucm')) {
+           $current[$menu] = $subm . $menu;
+       } elseif (!empty($system->modules['menu'][$menu])) {
+           $current[$menu] = $subm . $system->modules['menu'][$menu]['title'];
+       }
         }
     }
 }
 foreach ($skin['menu_point'] as $column => $text) {
     if(!isset($current['/' . $column])) {
-        $unused['/' . $column] = __('Column') . ': ' . $text;
+        $unused['/' . $column] = '<b>'.__('Column') . ': ' . $text.'</b>';
     }
 }
 
 foreach ($system->modules['menu'] as $menu => $data) {
-    if(!rcms_in_array_recursive(' > ' . $data['title'], $current)) {
-        $unused[$menu] = ' > ' . $data['title'];
+    if(!rcms_in_array_recursive($subm . $data['title'], $current)) {
+        $unused[$menu] = $subm . $data['title'];
     }
 }
 
 $ucms = ucm_list();
 foreach ($ucms as $menu=>$data) {
-    if(!rcms_in_array_recursive(' > ucm:' . $menu, $current)) {
-        $unused['ucm:' . $menu] = ' > ucm:' . $menu;
+    if(!rcms_in_array_recursive($subm . 'ucm:' . $menu, $current)) {
+        $unused['ucm:' . $menu] = $subm . 'ucm:' . $menu;
     }
 }
 ?>
-<script type="text/javascript" src="<?=ADMIN_PATH?>skins/slmv.js"></script>
-<form name="form1" onsubmit="on_submit_prepare(document.form1.elements['menus[]'])" action="" method="POST">
-<input type="hidden" name="save" value="1" />
+<style>
+#menus, #unused {
+	height:200px;
+	overflow-y:auto;
+}
+
+#menus li, #unused li {
+    font-size: 1.1em;
+    margin: 0 3px 3px;
+    padding: 3px;
+	list-style-type: none;
+	cursor: move;
+}
+</style>
+
 <table cellpadding="2" cellspacing="1" border="0" align="center" width="100%">
 <tr>
-    <td valign="top" align="center" class='row1' width="45%">
-        <select name="menus[]" size="15" style="width:100%" multiple>
-            <?php foreach ($current as $element => $text) echo '<option value="' . $element . '">' . $text . '</option>';?>
-        </select>
-    </td>
-    <td valign="top" align="center" class='row1'>
-        <input type="button" name="left_up" value="<?=__('Up')?>" onclick="select_move_up_selected_el(document.form1.elements['menus[]'])">
-        <input type="button" name="left_down" value="<?=__('Down')?>" onclick="select_move_down_selected_el(document.form1.elements['menus[]'])">
-        <input type="button" name="add" value="« <?=__('Add')?>" onclick="add_to_select_from_another(document.form1.elements['usused'], document.form1.elements['menus[]'])">
-        <input type="button" name="remove" value="<?=__('Delete')?> »" onclick="add_to_select_from_another(document.form1.elements['menus[]'], document.form1.elements['usused'])">
-    </td>
-    <td valign="top" align="center" class='row1' width="45%">
-        <?=__('Unused modules')?>
-        <select name="usused" size="14" style="width:100%">
-            <?php foreach ($unused as $element => $text) echo '<option value="' . $element . '">' . $text . '</option>';?>
-        </select>
-    </td>
+<th colspan="2">
+<?=__('Menus management')?>
+</th>
 </tr>
 <tr>
-    <td colspan="3" align="center" class='row2'><?=__('You can define position of menu modules in this form. There are two type of elements in fields: "columns" (Element of this type is followed by menu modules, that will be show in this field, until another "column" element) and menu modules. Elements in second field and menus before first "column" in first field will not be used.')?></td>
+    <th valign="top" align="left" class='row2' width="50%">
+	<?=__('Used modules')?>
+	</td>
+    <th valign="top" align="left" class='row2' width="50%">
+    <?=__('Unused modules')?>
+	</td>
 </tr>
 <tr>
-    <td colspan="3" align="center" class='row2'><input type="submit" name="" value="<?=__('Submit')?>"></td>
+	<td valign="top" align="left" class='row1'>	
+	<form name="form1"  action="" method="POST">
+	<input type="hidden" name="save" value="1" />
+        <ul id="menus" class="MenuSortable" >
+       <?php foreach ($current as $element => $text) echo '<li class="ui-state-highlight"><input type="hidden" name="menus[]" value="'.$element.'">'.$text.'</li>';?>
+        </ul>
+		<p>
+		<?=__('To enable the required modules drag modules from right to left field. Elements in second field and menus before first "column" in first field will not be used.')?>
+		</p>
+		<center>
+		<input type="submit" name="" value="<?=__('Save')?>"> <a href="admin.php?show=module&id=modules.menus&tab=6" class="button"><?=__('Clear')?></a>
+		</center>
+	</form>
+    </td>
+    <td valign="top" align="left" class='row1'>
+        <ul  id="unused" class="MenuSortable">
+       <?php foreach ($unused as $element => $text) echo '<li  class="ui-state-default"><input type="hidden" name="menus[]" value="' . $element . '">' . $text . '</li>';?>
+        </ul>
+    </td>
 </tr>
 </table>
-</form>
+
 <?php
 //Analog Widget Logic from WP
 $select_menus="<select name=\'modules[]\' onChange=\'if ($(this).val()!=0) $(this).prev().val($(this).val());\'>"
@@ -135,3 +156,11 @@ __('Module').' '.$frm->text_box('logic[modules][]', @$logic['modules'][$i], 15)
 }
 $frm->addmessage('<div id="add_logic"></div>'.$frm->hidden('settingschange', true));
 $frm->show();?>
+<script type="text/javascript">
+$(function(){
+     $("#menus, #unused").sortable({
+       connectWith: ".MenuSortable",
+       tolerance: "pointer"
+	   });
+});
+</script>
