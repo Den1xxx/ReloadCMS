0f5ccf715b9a7400e29951c0989f83d92404ccff
Den1xxx <from.gomel@gmail.com>
2014-05-05 10:29:07 +0300

    Sorting russian language vars
    Отсортированы переменные русского языка по алфавиту, чтобы проще было
    искать.

--- languages/russian/sendmail.php
+++ languages/russian/sendmail.php
@@ -1,22 +1,28 @@
-<?php 
-$lang['def']['Manage sendmail'] = 'Управление отправкой писем';
-$lang['def']['Sender name'] = 'Имя отправителя';
+<?php
+////////////////////////////////////////////////////////////////////////////////
+//   Copyright (C) ReloadCMS Development Team                                 //
+//   http://reloadcms.com                                                     //
+//   This product released under GNU General Public License v2                //
+////////////////////////////////////////////////////////////////////////////////
+
+$lang['def']['Admin e-mail is empty'] = 'E-mail администратора не введён';
+$lang['def']['Cannot open template for editing'] = 'Невозможно загрузить шаблон для редактирования';
 $lang['def']['Create sendmail template'] = 'Создание шаблона отправки писем';
 $lang['def']['Edit sendmail template'] = 'Редактирование шаблона отправки писем';
-$lang['def']['Sender email'] = 'Email отправителя';
+$lang['def']['Email for users letters'] = 'Электропочта для пользователей';
 $lang['def']['Error in email field'] = 'Ошибка email адреса';
 $lang['def']['Error of a control code'] = 'Ошибка контрольного кода';
-$lang['def']['It is important!'] = 'Это важно!';
 $lang['def']['Important'] = 'Важно';
-$lang['def']['Cannot open template for editing'] = 'Невозможно загрузить шаблон для редактирования';
+$lang['def']['It is important!'] = 'Это важно!';
 $lang['def']['Letter is empty'] = 'Вы забыли написать письмо';
+$lang['def']['Manage sendmail'] = 'Управление отправкой писем';
+$lang['def']['Sender email'] = 'Email отправителя';
+$lang['def']['Sender name'] = 'Имя отправителя';
 $lang['def']['Sendmail'] = 'Послать письмо';
 $lang['def']['Subject of your letter'] = 'Тема Вашего письма';
 $lang['def']['Text of your letter'] = 'Текст Вашего письма';
 $lang['def']['The letter to admin'] = 'Написать письмо администратору';
 $lang['def']['Your e-mail for answer'] = 'Ваш e-mail для ответа';
-$lang['def']['Email for users letters'] = 'Электропочта для пользователей';
 $lang['def']['Your letter will be sent to admin and it will not be kept on a site. Admin will answer to you as soon as he will have a possibility.'] = 'Ваше письмо будет послано администратору, и не будет сохранено на сайте. Администратор ответит Вам, как только у него будет возможность.';
 $lang['def']['Your name'] = 'Ваше имя';
-$lang['def']['Admin e-mail is empty'] = 'E-mail администратора не введён';
 ?>
--- languages/russian/skins.php
+++ languages/russian/skins.php
@@ -1,43 +1,49 @@
 <?php
-$lang['def']['This option can not be changed interactively'] = 'Этот параметр нельзя менять интерактивно';
-$lang['def']['Changes can be seen only after saving configuration and reloading the page'] = 'Изменения можно увидеть только после сохранения конфигурации и перезагрузки страницы';
-$lang['def']['Skin width, px'] = 'Ширина шаблона, px';
-$lang['def']['Sidebar width, px'] = 'Ширина боковой панели, px';
-$lang['def']['Header height, px'] = 'Высота шапки, px';
-$lang['def']['Logo padding, px'] = 'Отступ логотипа, px';
-$lang['def']['Primary font size, px'] = 'Основной размер шрифта, px';
-$lang['def']['Font family'] = 'Семейство шрифта';
-$lang['def']['Button text color'] = 'Цвет текста кнопки';
+////////////////////////////////////////////////////////////////////////////////
+//   Copyright (C) ReloadCMS Development Team                                 //
+//   http://reloadcms.com                                                     //
+//   This product released under GNU General Public License v2                //
+////////////////////////////////////////////////////////////////////////////////
+
 $lang['def']['Body background color'] = 'Основной цвет фона';
-$lang['def']['Text color'] = 'Цвет текста';
-$lang['def']['Title color'] = 'Цвет заголовков';
-$lang['def']['Link color'] = 'Цвет ссылок';
-$lang['def']['Link color when hovering'] = 'Цвет ссылок при наведении';
-$lang['def']['The thickness of the border of the menu, px'] = 'Толщина границы меню, px';
-$lang['def']['Menu radius, px'] = 'Радиус меню, px';
-$lang['def']['Padding links menu, px'] = 'Отступ ссылок меню, px';
-$lang['def']['Menu text color'] = 'Цвет текста меню';
-$lang['def']['Menu font size, px'] = 'Размер текста меню, px';
-$lang['def']['Sidebar position'] = 'Расположение боковой панели';
-$lang['def']['Sidebar title font size, px'] = 'Размер шрифта заголовка боковой панели, px';
-$lang['def']['Sidebar title radius, px'] = 'Радиус заголовка боковой панели, px';
-$lang['def']['Sidebar title padding, px'] = 'Размер отступа боковой панели, px';
-$lang['def']['Start menu gradient color'] = 'Начальный цвет градиента меню';
+$lang['def']['Button end gradient color'] = 'Конечный цвет градиента кнопок';
+$lang['def']['Button end gradient color hover'] = 'Конечный цвет градиента кнопок при наведении';
+$lang['def']['Button padding, px'] = 'Отступ кнопок, px';
+$lang['def']['Button start gradient color'] = 'Стартовый цвет градиента кнопок';
+$lang['def']['Button start gradient color hover'] = 'Стартовый цвет градиента кнопок при наведении';
+$lang['def']['Button text color'] = 'Цвет текста кнопки';
+$lang['def']['Cancel'] = 'Отмена';
+$lang['def']['Changes can be seen only after saving configuration and reloading the page'] = 'Изменения можно увидеть только после сохранения конфигурации и перезагрузки страницы';
 $lang['def']['End menu gradient color'] = 'Конечный цвет градиента меню';
-$lang['def']['Start menu gradient color hover'] = 'Начальный цвет градиента меню при наведении';
 $lang['def']['End menu gradient color hover'] = 'Конечный цвет градиента меню при наведении';
-$lang['def']['Input text color'] = 'Цвет текста поля ввода';
-$lang['def']['Input text hover color'] = 'Цвет текста поля ввода при наведении';
-$lang['def']['Input border thickness, px'] = 'Толщина границы поля ввода';
+$lang['def']['Font family'] = 'Семейство шрифта';
+$lang['def']['Header height, px'] = 'Высота шапки, px';
 $lang['def']['Input border color'] = 'Цвет границы поля ввода';
 $lang['def']['Input border hover color'] = 'Цвет границы поля ввода при наведении';
 $lang['def']['Input border radius, px'] = 'Радиус границы поля ввода';
+$lang['def']['Input border thickness, px'] = 'Толщина границы поля ввода';
 $lang['def']['Input padding, px'] = 'Отступ в поле ввода, px';
-$lang['def']['Button start gradient color'] = 'Стартовый цвет градиента кнопок';
-$lang['def']['Button end gradient color'] = 'Конечный цвет градиента кнопок';
-$lang['def']['Button start gradient color hover'] = 'Стартовый цвет градиента кнопок при наведении';
-$lang['def']['Button end gradient color hover'] = 'Конечный цвет градиента кнопок при наведении';
-$lang['def']['Button padding, px'] = 'Отступ кнопок, px';
-$lang['def']['Cancel'] = 'Отмена';
+$lang['def']['Input text color'] = 'Цвет текста поля ввода';
+$lang['def']['Input text hover color'] = 'Цвет текста поля ввода при наведении';
+$lang['def']['Link color'] = 'Цвет ссылок';
+$lang['def']['Link color when hovering'] = 'Цвет ссылок при наведении';
+$lang['def']['Logo padding, px'] = 'Отступ логотипа, px';
+$lang['def']['Menu font size, px'] = 'Размер текста меню, px';
+$lang['def']['Menu radius, px'] = 'Радиус меню, px';
+$lang['def']['Menu text color'] = 'Цвет текста меню';
+$lang['def']['Padding links menu, px'] = 'Отступ ссылок меню, px';
+$lang['def']['Primary font size, px'] = 'Основной размер шрифта, px';
 $lang['def']['Select'] = 'Выбор';
+$lang['def']['Sidebar position'] = 'Расположение боковой панели';
+$lang['def']['Sidebar title font size, px'] = 'Размер шрифта заголовка боковой панели, px';
+$lang['def']['Sidebar title padding, px'] = 'Размер отступа боковой панели, px';
+$lang['def']['Sidebar title radius, px'] = 'Радиус заголовка боковой панели, px';
+$lang['def']['Sidebar width, px'] = 'Ширина боковой панели, px';
+$lang['def']['Skin width, px'] = 'Ширина шаблона, px';
+$lang['def']['Start menu gradient color'] = 'Начальный цвет градиента меню';
+$lang['def']['Start menu gradient color hover'] = 'Начальный цвет градиента меню при наведении';
+$lang['def']['Text color'] = 'Цвет текста';
+$lang['def']['The thickness of the border of the menu, px'] = 'Толщина границы меню, px';
+$lang['def']['This option can not be changed interactively'] = 'Этот параметр нельзя менять интерактивно';
+$lang['def']['Title color'] = 'Цвет заголовков';
 ?>
