e44f31ef6ec4420b41b4835b644fb9e8602710aa
Denis Sivzov <from.gomel@gmail.com>
2014-06-04 17:50:30 +0300

    Fix encoding

--- admin/skins/default/skin.general.php
+++ admin/skins/default/skin.general.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>     
-    <title><?=__('Administration')?></title>
     <?rcms_show_element('meta')?>
+    <title><?=__('Administration')?></title>
 <link rel="stylesheet" href="<?=ADMIN_SKIN?>style.css" type="text/css">
 <!--[if IE]>
 <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
